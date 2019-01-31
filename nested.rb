3c6ec75566e89592bce66e94c5cdbfc65a444b68
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 12:18:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -125,4 +125,5 @@ def adding_to_dennis
      }
 
 		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		 programmer_hash
 end
